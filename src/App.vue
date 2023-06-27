8d11f4c3c138f4f8c70c78174a7e8df41baee86a
Fernando Caliço <fernando.carvalho.calico@gmail.com>
2023-06-27 20:11:30 -0300

    Exercício módulo 27 EBAC - Calculadora VUEJS - Ajuste Pasta

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { reactive } from "vue";
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+
+const estado = reactive({
+  valor1: 0,
+  valor2: 0,
+  resultado: 0,
+  operacao: '+',
+});
+
+function calcular() {
+  estado.valor1 = parseFloat(estado.valor1);
+  estado.valor2 = parseFloat(estado.valor2);
+
+  if (estado.operacao != '') {
+
+    if (estado.operacao === '+') {
+        estado.resultado = estado.valor1 + estado.valor2;
+      } else if (estado.operacao === '-') {
+          estado.resultado = estado.valor1 - estado.valor2;
+      } else if (estado.operacao === '*') {
+          estado.resultado = estado.valor1 * estado.valor2;
+      } else if (estado.operacao === '/') {
+          if (estado.valor2 != 0 ) {
+            estado.resultado = (estado.valor1 / estado.valor2);
+          } else if (estado.resultado === '' || estado.resultado === '0') { 
+            estado.resultado = "";
+          }
+      }    
+  }
+
+  if (estado.resultado > 0) {
+    estado.resultado = estado.resultado.toFixed(2);
+  }
+}
+
+function alteraValor1(evento) {
+  estado.valor1 = evento.target.value;
+  calcular();
+}
+
+function escolheOperacao(evento) {
+  estado.operacao = evento.target.value
+
+  calcular();
+}
+
+function alteraValor2(evento) {
+  estado.valor2 = evento.target.value;
+  
+  if (estado.operacao != '/' || (evento.target.value != 0 && evento.target.value != '')){
+    calcular();
+  } else {
+      estado.resultado = 0;
+      if (evento.target.value != '') {
+        alert('Na divisão, o segundo valor não pode ser zero!');
+      }
+  }
+}
+</script>
+
+<template>
+  <body>
+    <div class="container">
+      <Cabecalho />
+      <Formulario 
+        :calcular="calcular" 
+        :alteraValor1="alteraValor1" 
+        :alteraValor2="alteraValor2" 
+        :escolhaOperacao="escolheOperacao"
+        :operacao="estado.operacao"
+        :valor2="estado.valor2"
+        :resultado="estado.resultado"/>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+  body{
+    background-image: url(./images/fundo.jpg);
+    background-size: contain;
+    background-color:#0c4b52;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    align-items: center;
+  }  
+  .container {
+    background-color: #0c4b52;
+    color:white;
+    border-radius: 16px;
+    padding-top: 50px;  
+    padding-bottom: 50px;
+    text-align: center;
+  }
+</style>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>Calculadora de Valores</h1>
+    </header>
+</template>
+
+<style scoped>
+    h1 {
+        text-decoration: underline;
+        margin-bottom: 50px;
+    }
+</style>
+
